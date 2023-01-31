065d5833e5892f62e9e5a7a4c2fe3c2b5061dc18
JULIANA URIBE HENAO <julianauribe166425@correo.itm.edu.co>
2023-01-31 17:21:56 -0500

    fix - exception no controlada

--- cypress/e2e/step_definitions/order.js
+++ cypress/e2e/step_definitions/order.js
@@ -15,15 +15,28 @@ When(
 );
 
 When("adiciono toppings, comentario y agrego el producto a la canasta", () => {
-  cy.frameLoaded('[id="add-to-cart-modal"] > div[0]');
-  cy.iframe('[id="add-to-cart-modal"] > div[0]').find("#add-to-cart-button").then(function(t){
-     const txtButton = t.text()
-     expect(txtButton).to.contains('AÑADIR AL CARRITO');
-     cy.log(txtButton);
+
+  cy.get('.add-to-cart-modal.modal.hide.fade', { timeout: 5000 }).then(($dialog)=>{
+    cy.wrap($dialog).find("[data-id='93'] > .d-flex > .quantity-select-related-product-increase").click();
+    cy.get('[data-id="392"] > .d-flex > .quantity-select-related-product-increase').click();
+    cy.get('#options_additional_information').type("Prueba tecnica de Julian Novoa")
+    cy.get('#add-to-cart-button').click();
+    });
+});
+
+Then ("realizara el pago de manera exitosa", ()=> {
+  cy.get('#button-checkout-duna').click();
+  cy.get("[title='d_una_checkout']", { timeout: 35000 }).its("0.contentDocument.body").then(($bodyFrame) => {
+    cy.wrap($bodyFrame).find("[name='email']", { timeout: 35000 }).type("jnovoalujan@gmail.com");
   })
+});
+
+/*cy.frameLoaded('[id="add-to-cart-modal"] > div[0]');
+  cy.iframe('[id="add-to-cart-modal"] > div[0]').find("#add-to-cart-button").then(function(t){
+    
+  })*/
 
   //cy.get('#add-to-cart-button').click();
-});
 
 /*Otro ensayo con cypress-iframe
   cy.enter().then(getBody => {
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,3 +18,33 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable, promise) => {
+    // when the exception originated from an unhandled promise
+    // rejection, the promise is provided as a third argument
+    // you can turn off failing the test in this case
+    if (promise) {
+      return false
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
+  })
+
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
+
+  Cypress.on('uncaught:exception', (err, runnable) => {
+    // we expect a 3rd party library error with message 'list not defined'
+    // and don't want to fail the test so we return false
+    if (err.message.includes('list not defined')) {
+      return false
+    }
+    // we still want to ensure there are no other unexpected
+    // errors, so we let them fail the test
+  })
+  
+  
+  
